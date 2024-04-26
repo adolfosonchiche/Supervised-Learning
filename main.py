1056bff832decb67c3d3e513fee14dd90e00567f
adolfosonchiche <adolfoson36@gamil.com>
2024-04-26 16:44:40 -0600

    perceptron for AND - OR logic gates

--- main.py
+++ main.py
@@ -4,35 +4,30 @@ import numpy as np
 
 
 if __name__ == '__main__':
-
-    X = np.array([
+    #entrada de datos
+    data_in = np.array([
         [0, 0],
         [0, 1],
         [1, 0],
         [1, 1]
     ])
 
+    # perceptron para una compuerta OR
     ors = np.array([0, 1, 1, 1])
-    perceptron_or = Perceptron(2, 0.2, 100, LogicGatesEnum.OR)
-    perceptron_or.fit(X, ors)
+    perceptron_or = Perceptron(2, 0.3, 100, LogicGatesEnum.OR)
+    perceptron_or.fit(data_in, ors)
 
     print("Resultado del perceptrón con la compuerta OR:")
-    for x in X:
+    for x in data_in:
         print(x, "-->", perceptron_or.predict(x))
 
-    #AND
-    y = np.array([
-        [0, 0],
-        [0, 1],
-        [1, 0],
-        [1, 1]
-    ])
+    #perceptron para una compuerta AND
     ands = np.array([0, 0, 0, 1])
     perceptron_and = Perceptron(2, 0.2, 100, LogicGatesEnum.AND)
-    perceptron_and.fit(y, ands)
+    perceptron_and.fit(data_in, ands)
 
     print("Resultado del perceptrón con la compuerta AND:")
-    for x in X:
+    for x in data_in:
         print(x, "-->", perceptron_and.predict(x))
 
 
--- perceptron.py
+++ perceptron.py
@@ -16,12 +16,12 @@ class Perceptron:
         return 1 if x >= 0 else 0
 
     def predict(self, x):
-        if self.gate is LogicGatesEnum.OR:
+        if self.gate is LogicGatesEnum.OR or self.gate is LogicGatesEnum.AND:
             x = np.insert(x, 0, 1)
             z = self.weights.T.dot(x)
             a = self.activation_value(z)
             return a
-        elif self.gate is LogicGatesEnum.AND:
+        elif self.gate is LogicGatesEnum.NOT:
             z = np.dot(x, self.weights[1:]) + self.weights[0]
             a = self.activation_value(z)
             return a
@@ -33,17 +33,10 @@ class Perceptron:
             cont += 1
             self.errors = []
             for i in range(d.shape[0]):
-                if self.gate is LogicGatesEnum.OR:
-                    y = self.predict(x[i])
-                    e = d[i] - y
-                    self.weights = self.weights + self.lr * e * np.insert(x[i], 0, 1)
-                    self.errors.append(e)
-                elif self.gate is LogicGatesEnum.AND:
-                    x = np.insert(x[i], 0, 1)
-                    y = self.predict(x)
-                    e = d[i] - y
-                    self.weights[1:] += self.lr * e * x
-                    self.weights[0] += self.lr * e
+                y = self.predict(x[i])
+                e = d[i] - y
+                self.weights = self.weights + self.lr * e * np.insert(x[i], 0, 1)
+                self.errors.append(e)
             if all(error == 0 for error in self.errors):
                 print("Todos los elementos en 'errors' son cero")
                 break
