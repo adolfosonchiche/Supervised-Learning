507647991317842b2afdbc6226b8fb0c4ca5e65d
adolfosonchiche <adolfoson36@gamil.com>
2024-04-26 22:06:52 -0600

    random data

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,18 @@
+from random import random
+
 import numpy as np
 
 from logic_gates_enum import LogicGatesEnum
 
 
 class Perceptron:
-    def __init__(self, input_size, lr, epochs, gate):
+    def __init__(self, input_size, umbral, epochs, gate):
         self.epochs = epochs
         self.weights = np.zeros(input_size + 1)
-        self.lr = lr
+        self.weights = data_in = np.array([
+            random() for _ in range(input_size+1)
+        ])
+        self.umbral = umbral
         self.errors = []
         self.gate = gate
 
@@ -16,15 +21,10 @@ class Perceptron:
         return 1 if x >= 0 else 0
 
     def predict(self, x):
-        if self.gate is LogicGatesEnum.OR or self.gate is LogicGatesEnum.AND:
-            x = np.insert(x, 0, 1)
-            z = self.weights.T.dot(x)
-            a = self.activation_value(z)
-            return a
-        elif self.gate is LogicGatesEnum.NOT:
-            z = np.dot(x, self.weights[1:]) + self.weights[0]
-            a = self.activation_value(z)
-            return a
+        x = np.insert(x, 0, 1)
+        z = self.weights.T.dot(x)   #sumar sesgo
+        a = self.activation_value(z)
+        return a
 
     def fit(self, x, d):
         cont = 1
@@ -33,9 +33,9 @@ class Perceptron:
             cont += 1
             self.errors = []
             for i in range(d.shape[0]):
-                y = self.predict(x[i])
-                e = d[i] - y
-                self.weights = self.weights + self.lr * e * np.insert(x[i], 0, 1)
+                result = self.predict(x[i])
+                e = d[i] - result
+                self.weights = self.weights + self.umbral * e * np.insert(x[i], 0, 1)
                 self.errors.append(e)
             if all(error == 0 for error in self.errors):
                 print("Todos los elementos en 'errors' son cero")
